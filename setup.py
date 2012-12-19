d1bb4456a5b541a604e3374db8e0396a050c75c3
Samuel Charron <samuel.charron@gmail.com>
2012-12-19 14:41:39 +0100

    Add the python3 classification

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Mathematics', 
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
